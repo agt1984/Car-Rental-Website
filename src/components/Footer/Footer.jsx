901e18e35c1d37a0d5f838e2d8ff498cc1bcb9f9
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-05-11 16:34:17 +0200

    footer ready

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,125 @@
 import React from 'react'
 
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../../styles/footer.css";
+
+const quickLinks = [
+  {
+    path: "/about",
+    display: "Nosotros",
+  },
+  {
+    path: "#",
+    display: "Politica de Privacidad",
+  },
+  {
+    path: "/cars",
+    display: "Vehículos",
+  },
+  {
+    path: "/blogs",
+    display: "Blog",
+  },
+  {
+    path: "/contact",
+    display: "Contacto",
+  },
+];
+
+
 const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4" md="4" sm="12">
+            <div className="logo footer__logo">
+              <h1>
+                <Link to="/home" className=" d-flex align-items-center gap-2">
+                  <i class="ri-car-line"></i>
+                  <span>
+                    E-Fast <br />{" "}
+                    <spam style={{ fontSize: "17.5px" }}>
+                      Numero 1 en alquiler de coches electricos
+                    </spam>
+                  </span>
+                </Link>
+              </h1>
+            </div>
+            <p className="footer__logo-content">
+              En e-fast somos especialistas en alquiler de vehículos eléctricos.
+              Descubre la comodidad y rapidez que ofrecemos a nuestros clientes.
+              Ven a conocernos y disfruta de nuestra gama de vehículos
+              eléctricos.
+            </p>
+          </Col>
+
+          <Col lg="2" md="4" sm="6">
+            <div className="mb-4">
+              <h5 className="footer__link-title">Quick Links</h5>
+              <ListGroup>
+                {quickLinks.map((item, index) => (
+                  <ListGroupItem key={index} className="p-0 mt-3 quick__link">
+                    <Link to={item.path}>{item.display}</Link>
+                  </ListGroupItem>
+                ))}
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <div className="mb-4">
+              <h5 className="footer__link-title mb-4">Ven a conocernos en:</h5>
+              <p className="office__info">Av. de Sarrià, 150 08017 Barcelona</p>
+              <p className="office__info">Telefono: +664155888</p>
+
+              <p className="office__info">Email: info@e-fast.com</p>
+
+              <p className="office__info">
+                Horario comercial: <br />{" "}
+                <spam style={{ fontSize: "14.5px" }}>
+                  <b>Lunes a Viernes:</b> 09:00 – 14:00 y 16:00 – 20:00
+                </spam>{" "}
+                <br />{" "}
+                <spam style={{ fontSize: "14.5px" }}>
+                  <b>Sabados:</b> 10:00 a 13:00
+                </spam>
+                <br />{" "}
+                <spam style={{ fontSize: "14.5px" }}>
+                  <b>Domingos:</b> cerrado
+                </spam>
+              </p>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="12">
+            <div className="mb-4">
+              <h5 className="footer__link-title">Newsletter</h5>
+              <p className="section__description">Subscribe our newsletter</p>
+              <div className="newsletter">
+                <input type="email" placeholder="Email" />
+                <span>
+                  <i class="ri-send-plane-line"></i>
+                </span>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg="12">
+            <div className="footer__bottom">
+              <p className="section__description d-flex align-items-center justify-content-center gap-1 pt-4">
+                <i class="ri-copyright-line"></i>Copyright {year}, Developed by
+                .:Frater aGt:. All rights reserved.
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
 
 export default Footer
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
 import { Container, Row, Col } from 'reactstrap'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from "react-router-dom";
 import "../../styles/header.css";
 
+//links de navegacion del nav
+const navLinks = [
+  {
+    path: "/home",
+    display: "Inicio",
+  },
+  {
+    path: "/about",
+    display: "Nosotros",
+  },
+  {
+    path: "/cars",
+    display: "Vehículos",
+  },
+  {
+    path: "/blogs",
+    display: "Blog",
+  },
+  {
+    path: "/contact",
+    display: "Contacto",
+  },
+];
+
+//recuerda que aun falta los ejecutables de el onclick
 const Header = () => {
 
   return (
@@ -16,7 +41,7 @@ const Header = () => {
               <div className="header__top__left">
                 <span>¿Necesitas Ayuda?</span>
                 <span className="header__top__help">
-                  <i class="ri-phone-fill"></i> +33-666-777-1234
+                  <i class="ri-phone-fill"></i> +664155888
                 </span>
               </div>
             </Col>
@@ -36,6 +61,102 @@ const Header = () => {
         </Container>
       </div>
 
+      {/* =============== header middle =========== */}
+      <div className="header__middle">
+        <Container>
+          <Row>
+            <Col lg="4" md="3" sm="4">
+              <div className="logo">
+                <h1>
+                  <Link to="/home" className=" d-flex align-items-center gap-2">
+                    <i class="ri-car-line"></i>
+                    <span>
+                      E-Fast <br />{" "}
+                      <spam style={{ fontSize: "17.5px" }}>
+                        Numero 1 en alquiler de coches electricos
+                      </spam>
+                    </span>
+                  </Link>
+                </h1>
+              </div>
+            </Col>
+
+            <Col lg="3" md="3" sm="4">
+              <div className="header__location d-flex align-items-center gap-2">
+                <span>
+                  <i class="ri-earth-line"></i>
+                </span>
+                <div className="header__location-content">
+                  <h4>España</h4>
+                  <h6>Ciudad de Madrid, España</h6>
+                </div>
+              </div>
+            </Col>
+
+            <Col lg="3" md="3" sm="4">
+              <div className="header__location d-flex align-items-center gap-2">
+                <span>
+                  <i class="ri-time-line"></i>
+                </span>
+                <div className="header__location-content">
+                  <h4>De Viernes a Domingo</h4>
+                  <h6>10am - 7pm</h6>
+                </div>
+              </div>
+            </Col>
+
+            <Col
+              lg="2"
+              md="3"
+              sm="0"
+              className=" d-flex align-items-center justify-content-end "
+            >
+              <button className="header__btn btn ">
+                <Link to="/contact">
+                  <i class="ri-phone-line"></i> Solicitar una llamada
+                </Link>
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
+      {/* ========== main navigation =========== */}
+
+      <div className="main__navbar">
+        <Container>
+          <div className="navigation__wrapper d-flex align-items-center justify-content-between">
+            <span className="mobile__menu">
+              <i class="ri-menu-line"></i>
+            </span>
+
+            <div className="navigation">
+              <div className="menu">
+                {navLinks.map((item, index) => (
+                  <NavLink
+                    to={item.path}
+                    className={(navClass) =>
+                      navClass.isActive ? "nav__active nav__item" : "nav__item"
+                    }
+                    key={index}
+                  >
+                    {item.display}
+                  </NavLink>
+                ))}
+              </div>
+            </div>
+
+            <div className="nav__right">
+              <div className="search__box">
+                <input type="text" placeholder="Search" />
+                <span>
+                  <i class="ri-search-line"></i>
+                </span>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
     </header>
   );
 }
