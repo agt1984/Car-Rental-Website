2208719313b41c51e265c05faa9522c2707fe41d
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-06-25 15:32:02 +0200

    CommonSection added

--- src/components/UI/CommonSection.jsx
+++ src/components/UI/CommonSection.jsx
@@ -1,10 +1,13 @@
 
 import React from 'react'
+import { Container } from 'reactstrap' 
 
 const CommonSection = () => {
   return (
     <section className='common__section'>
-
+        <Container className='text-center'>
+            <h1 className='text-ligth'>{title}</h1>
+        </Container>
     </section>
   )
 }
