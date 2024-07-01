d67be00d12ae0f9581480578ba25778199ec7f8d
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-07-01 18:15:52 +0200

    descriptions modified

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -28,21 +28,24 @@ const About = () => {
             <Col lg="6" md="6" sm="12">
               <div className="about__page-content">
                 <h2 className="section__title">
-                  We Are Committed To Provide Safe Ride Solutions
+                  Comprometidos a llevarte a donde Sea
                 </h2>
 
                 <p className="section__description">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Eveniet veniam assumenda aperiam accusantium ex autem
-                  perferendis repellendus nostrum delectus. Nemo et dolore est
-                  tempore rem minima adipisci magni dolorum ipsam.
+                  En nuestra empresa, estamos comprometidos a llevarte a donde
+                  sea con el máximo confort y seguridad. Nos esforzamos por
+                  ofrecer un servicio de transporte que no solo cumpla, sino que
+                  supere tus expectativas, garantizando puntualidad, eficiencia
+                  y una experiencia de viaje placentera. Ya sea que necesites
+                  desplazarte por negocios, placer o cualquier otra razón,
+                  nuestro equipo de profesionales dedicados está listo para
+                  asistir en cada paso del camino. Con nosotros, tu destino está
+                  asegurado, y cada viaje se convierte en una oportunidad para
+                  brindarte lo mejor
                 </p>
 
                 <p className="section__description">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Eveniet veniam assumenda aperiam accusantium ex autem
-                  perferendis repellendus nostrum delectus. Nemo et dolore est
-                  tempore rem minima adipisci magni dolorum ipsam.
+                  No temas, no te llevaremos a terrenos solos en donde te haremos cosas inpènsables, ni tampoco venderemos tus organos.
                 </p>
 
                 <div className=" d-flex align-items-center gap-3 mt-4">
@@ -51,7 +54,7 @@ const About = () => {
                   </span>
 
                   <div>
-                    <h6 className="section__subtitle">Need Any Help?</h6>
+                    <h6 className="section__subtitle">Necesitas Ayuda?</h6>
                     <h4>+00123456789</h4>
                   </div>
                 </div>
